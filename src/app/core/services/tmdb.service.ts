aeb11cedc7afc665ce03c1109004d8de29406207
Noel Sebastian <noelsimc69@gmail.com>
2025-08-08 16:54:34 +1000

    feat: integrate TMDB service for movie search functionality and update app configuration

---
+++ src/app/core/services/tmdb.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { environment as baseEnv } from '../../../environments/environment';
+
+import { ResourceService } from '@infrastructure/http';
+import { ApiResult, MovieResult, MovieDetails } from '@infrastructure/models';
+
+const env = { ...baseEnv };
+
+@Injectable({ providedIn: 'root' })
+export class TmdbService extends ResourceService {
+  // We don’t have a fixed resourcePath (varies: /movie, /search/movie, etc.)
+  constructor() {
+    super('', {
+      Authorization: `Bearer ${env.tmdbApiToken}`,
+    });
+  }
+
+  // Override base to TMDB base (not your app API)
+  protected override baseUrl = env.apiUrl.replace(/\/+$/, '');
+
+  // --- API methods ---
+
+  /** GET /movie/popular */
+  getPopularMovies(page = 1) {
+    return this.request<ApiResult<MovieResult>>(
+      'GET',
+      this.url('movie', 'popular'),
+      {
+        params: { page },
+      },
+    );
+  }
+
+  /** GET /movie/{id} */
+  getMovie(id: number) {
+    return this.request<MovieDetails>('GET', this.url('movie', id), {});
+  }
+
+  /** GET /search/movie?query=... */
+  searchMovies(query: string, page = 1) {
+    return this.request<ApiResult<MovieResult>>(
+      'GET',
+      this.url('search', 'movie'),
+      {
+        params: { query, page, include_adult: false },
+      },
+    );
+  }
+}
